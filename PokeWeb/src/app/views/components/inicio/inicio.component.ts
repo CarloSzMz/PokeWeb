674a78e6813389e6c2392ed962624583bf5a9ce1
JorgeGC03 <jorgegalacaballero@gmail.com>
2024-09-12 20:59:47 +0200

    Started framework creation

--- PokeWeb/src/app/views/components/inicio/inicio.component.ts
+++ PokeWeb/src/app/views/components/inicio/inicio.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
 
